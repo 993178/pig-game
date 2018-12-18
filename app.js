a9ccd40f6c2f0b1746295b86a1d60ce0648b6beb
993178 <papilio29@hotmail.com>
2018-12-18 02:50:02 +0100

    knor knor

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer; // het is maar 1 dobbelsteen... *lacht onschuldig*
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none'; // we selecteren een element, nl een div genaamd dice (zie die CSS-selector), gaan aan de CSS morrelen (style) en wel aan property 'display', die we op 'none' zetten
+document.getElementById('score-0').textContent = '0';   // een ID-element direct als ID-element zoeken is sneller dan querySelector. 
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {          // ipv een aparte functie met naam ergens te definiëren en die dan hier te noemen (niet callen) als callbackfunctie gooien we de hele (anonieme) functie gewoon in z'n geheel hierin, aangezien we hem toch maar 1x gaan gebruiken
+    // willekeurig getal tussen 1 en 6. We gebruiken 'die' alleen hier, dus die hoeft niet in de global scope
+    var die = Math.ceil(Math.random() * 6);         // het is maar één dobbelsteen... *lacht onschuldig*
+    
+    var diceDOM = document.querySelector('.dice')       // steeds weer zo'n ding selecteren kost moeite, dus gooien we hem in een variabele zodat we daarna die kunnen gebruiken (zie volgende regel)
+    diceDOM.style.display = 'block';        // dobbelsteen zichtbaar maken
+    diceDOM.src = 'dice-' + die + '.png';            // dobbelsteen met juiste hoeveelheid stippen doorgeven aan DOM, met gebruikmaking van JS type coercion
+
+
+    // update roundScore, maar alleen als getal niet 1 is
+    if (die !== 1) {
+        return roundScore += die;       // met alleen + krijg je alleen de laatste gooi plus de huidige gooi, maar je wilt ze ook alledrie/-vier/etc, sukkel
+    } else {
+        return roundScore = 0;
+    }
+})
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = die;   // Je zoekt&selecteert het juiste html-element - (type coercion maakt dat je '#current-' + variabele kunt schrijven en ermee wegkomt) - en dat textContent is een methode om tekst in te voegen (maar ALLEEN tekst). Dat stel je dan gelijk aan de uitkomst van var die.  = setter
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + die + '</em>';   // tweede manier, waarmee je ook html kunt toevoegen aan je tekstelement (maar daar heb je toch CSS voor??).
+
+
+// var x = document.querySelector('#score-0').textContent;  // om een score te lezen en in een variabele op te slaan. Behalve dat alles wat daar staat uiteindelijk ook hier vandaan komt, dus...         = getter
